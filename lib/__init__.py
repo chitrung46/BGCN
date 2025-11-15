28d4a632b2281d277c475b9f43700976577cdbff
chitrung46 <luongchitrung53@gmail.com>
2025-11-16 00:13:20 +0700

    update code

--- lib/__init__.py
+++
@@ -1,9 +0,0 @@
-from .SRTrainer import SRTrainer
-
-TRAINERS = {
-    SRTrainer.code(): SRTrainer
-}
-
-def trainer_factory(args, model, train_loader, val_loader, test_loader):
-    trainer = TRAINERS[args.trainer_code]
-    return trainer(args, model, train_loader, val_loader, test_loader)
