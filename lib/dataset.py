723473d66bfb9b95c03f341f0b0b705407500c7b
chitrung46 <luongchitrung53@gmail.com>
2025-11-17 19:58:00 +0700

    update code

--- lib/dataset.py
+++ lib/dataset.py
@@ -32,16 +32,13 @@ def normalize_seq(seqs, min_seq_len=0, max_seq_len=None):
     
     filtered_seqs = dict()
     masks = dict()
-    padding = [(0, '', pd.Timestamp(0))]
+    padding = [0, '', pd.Timestamp(0)]
 
-    for uidx, seq in seqs.items():
+    for idx, seq in enumerate(seqs.values()):
         if len(seq) >= min_seq_len:
             l = len(seq)
-            filtered_seqs[uidx] = [padding*(max_len-l) + seq[-max_len:] if (max_len > l) 
-                                    else seq[-max_len:]]
-            masks[uidx] = [[0]*(max_len-l) + [1]*max_len if (max_len > l) 
-                                    else [1]*max_len]
-
+            filtered_seqs[idx] = [padding]*(max_len-l) + seq[-max_len:] if (max_len > l) else seq[-max_len:]
+            masks[idx] = [0]*(max_len-l) + [1]*max_len if (max_len > l) else [1]*max_len
     
     print(f"Average length of sequence: {sum(seq_len)/len(seq_len)}")
     print(f"Maximum length of sequence: {max_len}")
